279adb947e97ce311898342eef30da00c5f3c438
Navneet Patel <navneetpatel325@gmail.com>
2026-05-16 16:38:51 +0530

    Feature section added

--- Frontend/src/pages/LandingPage/LandingPage.jsx
+++ Frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../../components/landing/Header'
 import Hero from '../../components/landing/Hero'
+import Features from '../../components/landing/Features'
 
 const LandingPage = () => {
   return (
     <div className='bg-[#ffffff] text-gray-600 '>
         <Header/>
-        <main>
+        <main className='mb-[100vh]'>
           <Hero/>
+          <Features/>
         </main>
     </div>
   )
---
+++ Frontend/src/utils/data.js
@@ -0,0 +1,26 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+
+export const FEATURES = [
+    {
+        icon : Sparkles,
+        title : "AI Invoice Creation",
+        description : "Paste any text , email or receipt, and let our  AI instantly generate a complete professional invoice for you"
+    },
+     {
+        icon : BarChart2,
+        title : "AI-Powered DashBoard",
+        description : "Get smart, actionable insights about your business finances, generated automatically by our AI analyst"
+    },
+     {
+        icon : Mail,
+        title : "Smart Reminders",
+        description : "Automatically generate polite and effective payment reminder emails for overdue invoices with a single click"
+    },
+     {
+        icon : FileText,
+        title : "Easy Invoice Management",
+        description : "Easily manage all your invoices, track payments, and send reminders for overdue payments"
+    },
+
+]
