54c6515df330269725c55b8965e6ef5e0579dc3e
patelscripts <navneetpatel325@gmail.com>
2026-07-25 13:34:56 +0530

    alignment issue fix

--- Frontend/src/pages/Invoices/AllInvoices.jsx
+++ Frontend/src/pages/Invoices/AllInvoices.jsx
@@ -175,7 +175,7 @@ const AllInvoices = () => {
                   <th className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>Amount</th>
                   <th className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>Due Date</th>
                   <th className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>Status</th>
-                  <th className='px-6 py-3 text-left text-xs font-medium text-slate-500 uppercase tracking-wider'>Actions</th>
+                  <th className='px-6 py-3 text-right text-xs font-medium text-slate-500 uppercase tracking-wider'>Actions</th>
                 </tr>
               </thead>
 
