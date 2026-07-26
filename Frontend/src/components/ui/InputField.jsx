f905694d001c8077612e03344a57b795f20949a4
patelscripts <navneetpatel325@gmail.com>
2026-07-26 14:33:32 +0530

    profile page completed

--- Frontend/src/components/ui/InputField.jsx
+++ Frontend/src/components/ui/InputField.jsx
@@ -3,7 +3,7 @@ function InputField({icon:Icon, label, name, ...props}){
         <div>
             <label htmlFor={name} className="block text-sm font-medium text-slate-700 mb-2">{label}</label>
             <div className="relative">
-                {Icon && <div className="absolute inste-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                {Icon && <div className="absolute inste-y-0 left-0 pt-2 pl-2 flex items-center pointer-events-none">
                    <Icon className="w-5 h-5 text-slate-400"/> 
                    </div>}
                    <input 
